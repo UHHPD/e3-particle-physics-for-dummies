c4e70f16feaa4d63048c41ba2e38850172733678
luzio33 <54103941+luzio33@users.noreply.github.com>
2021-11-03 14:28:53 +0000

    1a go brr

--- poisson.cc
+++ poisson.cc
@@ -1,20 +1,25 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+using namespace std;
 
 
 double poisson(double mu, int k) {
     return 0;
 }
-
+double daten[234];
 int main() {
-    using namespace std;
+    std::vector<int> zaehler(11);
 
 
     ifstream fin("datensumme.txt");
     int n_i;
     for(int i = 0 ; i < 234 ; ++i) {
-        fin >> n_i;
+        fin >> daten[i];  
+        zaehler[daten[i]]+=1;
+    }
+    for ( unsigned int k = 0 ; k < zaehler . size () ; ++ k ) {
+    std :: cout << k << ":" << zaehler [ k ] << std :: endl ;
     }
     fin.close();
 }
