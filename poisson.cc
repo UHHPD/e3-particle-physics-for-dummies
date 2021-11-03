dae344707b0f4d94c52d6e54130ecd41c1bf7b40
luzio33 <54103941+luzio33@users.noreply.github.com>
2021-11-03 15:23:39 +0000

    3.1 done, bis auf bild

--- poisson.cc
+++ poisson.cc
@@ -1,19 +1,23 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <math.h>
 using namespace std;
 
-
-double poisson(double mu, int k) {
-    return 0;
-}
 double daten[234];
+
+double poisson(double mu, int k){
+  double p;
+  p=(pow(mu,k)*exp(-mu)/(tgamma(k+1)));
+}
+
 int main() {
     std::vector<int> zaehler(11);
 
 
     ifstream fin("datensumme.txt");
-    ofstream fout("histpoi.txt");
+    ofstream fout("hist.txt");
+    ofstream fout1("histpoi.txt");
     int n_i;
     for(int i = 0 ; i < 234 ; ++i) {
         fin >> daten[i];  
@@ -23,6 +27,11 @@ int main() {
     std :: cout << k << ":" << zaehler [ k ] << std :: endl ;
     fout << k << " " << zaehler [ k ] << std :: endl ;
     }
+
+  
+  for ( unsigned int k = 0 ; k < zaehler . size () ; ++ k ) {
+    fout1 << k << " " << zaehler [ k ]<< " " << 234*poisson(3.11538,k) << std :: endl ;
+  }
 
     fout.close();
     fin.close();
